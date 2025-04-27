1858d685e65e23454cbfe9bcda1c79856e2452a1
Maya Stein <mayarosestein@gmail.com>
2025-04-27 16:17:28 -0400

    Completed the lesson by adding s form for adding new friends to the list

--- Birthdays App/ContentView.swift
+++ Birthdays App/ContentView.swift
@@ -8,14 +8,43 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var friends: [Friend] = [
+        Friend(theName: "Ria Koppikar", theBirthday: Date(timeIntervalSinceReferenceDate:105236400)),
+        Friend(theName:"Kirsten Mahler", theBirthday: Date(timeIntervalSinceReferenceDate:111279600))
+    ]
+    @State private var newFriendName = ""
+    @State private var newFriendBirthday=Date.now
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack{
+            List(friends, id:\.name){ friend in
+                HStack{
+                    Text(friend.name)
+                    Spacer()
+                    Text(friend.birthday, format: .dateTime.month(.wide).day().year())
+                }
+            }
+            .navigationTitle("Birthdays")
+            .safeAreaInset(edge:.bottom) {
+                VStack(alignment:.center, spacing:20){
+                    Text("New Birthday").font(.headline)
+                    DatePicker(selection: $newFriendBirthday, in: Date.distantPast...Date.now, displayedComponents: .date) {
+                               TextField("Name", text: $newFriendName)
+                                   .textFieldStyle(.roundedBorder)
+                           }
+                    Button("Save"){
+                        let newFriend=Friend(theName: newFriendName, theBirthday: newFriendBirthday)
+                        friends.append(newFriend)
+                        newFriendName=""
+                        newFriendBirthday = .now
+                    }
+                    .bold()
+                    
+                }
+                .padding()
+                .background(.bar)
+               
+            }
         }
-        .padding()
     }
 }
 
---
+++ Birthdays App/Friend.swift
@@ -0,0 +1,17 @@
+//
+//  Friend.swift
+//  Birthdays App
+//
+//  Created by Maya Stein on 4/25/25.
+//
+
+import Foundation
+
+class Friend{
+    var name:String
+    var birthday:Date
+    init(theName: String, theBirthday: Date) {
+        name=theName
+        birthday=theBirthday
+    }
+}
